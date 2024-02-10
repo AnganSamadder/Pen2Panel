4f4124f3edbd3bc7d598ff99de546529b2d3d5f2
rohitgogi <rohit.gogi12@gmail.com>
2024-02-10 11:58:56 -0500

    button added

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,15 +1,26 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../../components/GetStartedButton";
 
 export default function Landing({}) {
   return (
     <div className="min-h-screen relative">
-    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-65 z-10"></div>
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
-      <Image src="/logo.png" alt="Logo" width={780} height={260} />
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-65 z-10"></div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+        <Image src="/logo.png" alt="Logo" width={780} height={260} />
+      </div>
+      <a href="/api/auth/login">
+        <button className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-7 rounded-full border border-white border-opacity-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/2 z-30 mt-4">
+          Get Started
+        </button>
+      </a>
+      <video
+        className="w-full h-full object-cover fixed top-0 left-0 z-0"
+        src="/videoBg.mp4"
+        autoPlay
+        loop
+        muted
+      />
     </div>
-    <video className="w-full h-full object-cover fixed top-0 left-0 z-0" src="/videoBg.mp4" autoPlay loop muted />
-  </div>
-
-
   );
 }
