e713e6b794f77d82e08da3192d69febe12ec5e6b
Angan Samadder <rio.angan@gmail.com>
2024-02-10 10:10:42 -0500

    add empty page files

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd0b1f7a7ba808f0"; }
+/******/ 		__webpack_require__.h = function() { return "afe239fc619e6293"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
